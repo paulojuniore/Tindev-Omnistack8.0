2c81b588c300569b96eb13f26c92a5b721d1d37b
paulojuniore <paulo.junior@ccc.ufcg.edu.br>
2019-08-09 10:53:53 -0300

    Adicionando mongoDB e configurando a string de conexão no servidor.

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,9 +3,12 @@ const express = require('express');
 const routes = express.Router();
 
 routes.get('/', (req, res) => {
-  return res.json({message: `Olá ${req.query.name}!`});
+    return res.json({ message: `Olá ${req.query.name}!` });
+})
+
+routes.post('/devs', (req, res) => {
+    console.log(req.body)
+    return res.json({ ok: true })
 })
 
 module.exports = routes;
-
-
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,12 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const routes = require('./routes')
 
 const server = express()
 
+mongoose.connect('mongodb+srv://paulo-jr:12345@cluster0-f7ti6.mongodb.net/omnistack?retryWrites=true&w=majority')
+
+server.use(express.json())
 server.use(routes)
 
 server.listen(3333)
