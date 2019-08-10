ad8678124d90a15c344e3f183185ecea06fe67d1
paulojuniore <paulo.junior@ccc.ufcg.edu.br>
2019-08-10 19:38:24 -0300

    Sistema de rotas adicionado e usando state para obter e setar valores.

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,14 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.css'
 
 import logo from '../assets/logo.svg'
 
-export default function Login() {
+export default function Login({ history }) {
+    const [username, setUsername] = useState('')  
+    const changeSubmit = evt => {
+        evt.preventDefault()
+        history.push('/main')
+        console.log(username)
+    }
     return (
         <div className='login-container'>
-            <form>
+            <form onSubmit={changeSubmit}>
                 <img src={logo} alt='Tindev' />
-                <input placeholder='Seu usuário do Github' />
+                <input 
+                    placeholder='Seu usuário do Github' 
+                    value={username}
+                    onChange={ evt => setUsername(evt.target.value) }
+                />
                 <button type='submit'>Login</button>
             </form>
         </div>
