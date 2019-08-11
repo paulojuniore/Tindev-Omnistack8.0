a66be6a56fe76c47ffecfe85bf15b190e7cfbdc0
paulojuniore <paulo.junior@ccc.ufcg.edu.br>
2019-08-10 21:20:33 -0300

    Fazendo request na api passando o id do dev, e estilizando a página principal de listagem dos devs.

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,15 +1,21 @@
 import React, { useState } from 'react'
 import './Login.css'
+import api from '../services/api'
 
 import logo from '../assets/logo.svg'
 
 export default function Login({ history }) {
     const [username, setUsername] = useState('')  
-    const changeSubmit = evt => {
+
+    const changeSubmit = async (evt) => {
         evt.preventDefault()
-        history.push('/main')
-        console.log(username)
+        const response = await api.post('/devs', {
+            username
+        })
+        const { _id } = response.data
+        history.push(`/dev/${_id}`)
     }
+
     return (
         <div className='login-container'>
             <form onSubmit={changeSubmit}>
--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,7 +1,76 @@
 import React from 'react'
+import './Main.css'
 
-export default function Main() {
+import logo from '../assets/logo.svg'
+import like from '../assets/like.svg'
+import dislike from '../assets/dislike.svg'
+
+export default function Main({ match }) {
     return (
-        <h1>Hello world!</h1>
+        <div className='main-container'>
+            <img src={logo} alt='' />
+            <ul>
+                <li>
+                    <img src='https://avatars2.githubusercontent.com/u/34824189?v=4' alt=''/>
+                    <footer>
+                        <strong>Paulo Júnior</strong>
+                        <p>A nice guy.</p>
+                    </footer>
+                    <div className='buttons'>
+                        <button type='button'>
+                            <img src={like} alt='' />
+                        </button>
+                        <button type='button'>
+                            <img src={dislike} alt='' />
+                        </button>
+                    </div>
+                </li>
+                <li>
+                    <img src='https://avatars2.githubusercontent.com/u/34824189?v=4' alt=''/>
+                    <footer>
+                        <strong>Paulo Júnior</strong>
+                        <p>A nice guy.</p>
+                    </footer>
+                    <div className='buttons'>
+                        <button type='button'>
+                            <img src={like} alt='' />
+                        </button>
+                        <button type='button'>
+                            <img src={dislike} alt='' />
+                        </button>
+                    </div>
+                </li>
+                <li>
+                    <img src='https://avatars2.githubusercontent.com/u/34824189?v=4' alt=''/>
+                    <footer>
+                        <strong>Paulo Júnior</strong>
+                        <p>A nice guy.</p>
+                    </footer>
+                    <div className='buttons'>
+                        <button type='button'>
+                            <img src={like} alt='' />
+                        </button>
+                        <button type='button'>
+                            <img src={dislike} alt='' />
+                        </button>
+                    </div>
+                </li>
+                <li>
+                    <img src='https://avatars2.githubusercontent.com/u/34824189?v=4' alt=''/>
+                    <footer>
+                        <strong>Paulo Júnior</strong>
+                        <p>A nice guy.</p>
+                    </footer>
+                    <div className='buttons'>
+                        <button type='button'>
+                            <img src={like} alt='' />
+                        </button>
+                        <button type='button'>
+                            <img src={dislike} alt='' />
+                        </button>
+                    </div>
+                </li>
+            </ul>
+        </div>
     )
 }
