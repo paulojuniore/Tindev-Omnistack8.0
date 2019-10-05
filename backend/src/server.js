ab07e4a49d7344524eecca8d4655e2b00706d0fc
paulojuniore <paulo.junior@ccc.ufcg.edu.br>
2019-10-05 18:21:07 -0300

    Preparando o backend para aceitar requisições http e websocket.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,20 @@
 const express = require('express')
 const mongoose = require('mongoose')
+
 const cors = require('cors')
 
 const routes = require('./routes')
 
-const server = express()
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
 
 mongoose.connect('mongodb+srv://paulo-jr:12345@cluster0-f7ti6.mongodb.net/omnistack?retryWrites=true&w=majority', {
     useNewUrlParser: true
 })
 
-server.use(cors())
-server.use(express.json())
-server.use(routes)
+app.use(cors())
+app.use(express.json())
+app.use(routes)
 
 server.listen(3333)
